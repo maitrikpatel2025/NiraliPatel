4a9b00267265f5f14a2cf71356d8213537d1f566
Maitrik Patel <59853047+maitrikpatel2025@users.noreply.github.com>
2020-02-28 23:18:14 -0500

    upadate

--- app/src/Components/Logo/Logo.js
+++ app/src/Components/Logo/Logo.js
@@ -3,10 +3,8 @@ import './Logo.css';
 
 const Logo =()=> {
 return(
-  <a href="/" > 
     <img src="https://www.vectorlogo.zone/logos/udemy/udemy-ar21.svg"
     alt="Udemy" width="110" height="32"/>
-  </a>
 )
 }
 export default Logo;
--- app/src/Components/TopNavBar/TopNavBar.js
+++ app/src/Components/TopNavBar/TopNavBar.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav';
-
-import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../Logo/Logo';
 import Btn from '../Button/Btn';
 import './TopNavBar.css';
 
 const TopNavBar = () => {
  return (
-    <Navbar className="TopNav">
-         <Logo/>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-         <Navbar.Collapse id="responsive-navbar-nav">
-           <Nav className="mr-auto">
-            <Btn Name="Home"></Btn>
-            <Btn Name="About us"></Btn>
-            <Btn Name="Service"></Btn>
-            <Btn Name="Testimonial"></Btn>
-            <Btn Name="Contact Us"></Btn>
-           </Nav>
-         </Navbar.Collapse>
-  </Navbar>
+    <nav className="TopNav navbar navbar-expand-lg">
+      <a className="navbar-brand" href="#"><Logo/></a>
+        <button className="navbar-toggler" type="button"
+         data-toggle="collapse" 
+         data-target="#navbarTogglerDemo02" 
+         aria-controls="navbarTogglerDemo02"
+         aria-expanded="false" aria-label="Toggle navigation">
+         <i className="bars icon"></i>
+         </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="Element navbar-nav mr-auto">
+         <Btn Name="home"/>
+         <Btn Name="home"/>
+         <Btn Name="home"/>
+         <Btn Name="home"/>
+         <Btn Name="home"/>
+        </ul>
+      </div>
+    </nav>
   )
 }
 export default TopNavBar;
